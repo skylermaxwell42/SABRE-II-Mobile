8173001d3a625abd4c70d0f93c784ee7919abee5
Skyler Maxwell <skylermaxwell42@gmail.com>
2019-02-23 18:46:07 -0500

    Adding JukeBoxViewController

--- SABRE-II-Mobile/ViewController.swift
+++ SABRE-II-Mobile/ViewController.swift
@@ -8,15 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-  @IBOutlet weak var jukeboxName: UITextField!
+class ViewController: UIViewController, UITextFieldDelegate {
+  
+  // Properties
+  @IBOutlet weak var jukeBoxUILabel: UILabel!
+  @IBOutlet weak var jukeBoxCreateButton: UIButton!
+  @IBOutlet weak var jukeBoxCreateTextField: UITextField!
+  @IBOutlet weak var jukeBoxCreateView: UIStackView!
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    // Handle the text field’s user input through delegate callbacks.
+    jukeBoxCreateTextField.delegate = self
   }
 
   @IBAction func jukeBoxCreateButton(_ sender: UIButton) {
+    return;
   }
+  
+  func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+    // Hide the keyboard.
+    textField.resignFirstResponder()
+    return true
+  }
+  
+  func textFieldDidEndEditing(_ textField: UITextField) {
+    jukeBoxUILabel.text!.append(jukeBoxCreateTextField.text!)
+    jukeBoxCreateTextField.text = ""
+    jukeBoxCreateTextField.placeholder = "Enter song name"
+    jukeBoxCreateButton.setTitle("Add Song", for: UIControl.State.normal)
+    
+    connectToServer()
+    performSegue(withIdentifier: "createRoomSegueue", sender: textField)
+  }
+  
+  func connectToServer() {
+    print("Connecting to sevrer")
+  }
+  
 }
 
