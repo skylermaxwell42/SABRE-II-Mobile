76502b58662897981b4651c0f9377fa26dd24796
Skyler Maxwell <skylermaxwell42@gmail.com>
2019-02-23 12:45:31 -0500

    Initial Commit

---
+++ SABRE-II-MobileTests/SABRE_II_MobileTests.swift
@@ -0,0 +1,34 @@
+//
+//  SABRE_II_MobileTests.swift
+//  SABRE-II-MobileTests
+//
+//  Created by Skyler Maxwell on 2/23/19.
+//  Copyright © 2019 SABRE. All rights reserved.
+//
+
+import XCTest
+@testable import SABRE_II_Mobile
+
+class SABRE_II_MobileTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
