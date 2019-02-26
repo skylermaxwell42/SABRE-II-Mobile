a63ddf85ef4c762e73f7d547e9dfc72ec4de99d4
Skyler Maxwell <skylermaxwell42@gmail.com>
2019-02-26 08:45:26 -0500

    Adding additional models post mordem

---
+++ SABRE-II-Mobile/Models/AppleMusicPlayer.swift
@@ -0,0 +1,9 @@
+//
+//  AppleMusicPlayer.swift
+//  SABRE-II-Mobile
+//
+//  Created by Skyler Maxwell on 2/26/19.
+//  Copyright © 2019 SABRE. All rights reserved.
+//
+
+import Foundation
---
+++ SABRE-II-Mobile/Models/JukeBoxMusicPlayer.swift
@@ -0,0 +1,8 @@
+//
+//  JukeBoxMusicPlayer.swift
+//  
+//
+//  Created by Skyler Maxwell on 2/26/19.
+//
+
+import Foundation
